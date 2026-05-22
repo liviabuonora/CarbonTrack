ce296b60452050904de12006dfe692501a5f0504
Jullya Medeiros <juuvmed@gmail.com>
2026-05-22 09:44:51 -0300

    Atualiza menu

--- src/main.py
+++ src/main.py
@@ -1,12 +1,68 @@
-from database import conectar
+from database import criar_banco
+from empresa import cadastrar_empresa
+from fonte import cadastrar_fonte, listar_fontes, editar_fonte, desativar_fonte
 
-connect = conectar()
+def menu_fontes(empresa_id):
+    while True:
+        print("\n___Fontes de Emissão___")
+        print("[1] Cadastrar nova fonte")
+        print("[2] Listar fontes ativas")
+        print("[3] Editar fonte")
+        print("[4] Desativar fonte")
+        print("[5] Voltar")
 
-with open("database/schema.sql", "r", encoding ="utf-8") as arquivo:
-    sql = arquivo.read()
+        opcao = input("\nEscolha: ").strip()
 
-connect.executescript(sql)
+        
+        if opcao == "3":
 
-print("Banco criado")
+            listar_fontes(empresa_id)
 
-connect.close()
+            fonte_id = int(input("Insira o ID da fonte que deseja alterar: "))
+            novo_nome = input("Insira o novo nome da fonte: ").strip()
+            novo_tipo = input("Insira o novo tipo da fonte: ").strip()
+            unidade = input("Insira a unidade: ").strip()
+            editar_fonte(fonte_id,novo_nome,novo_tipo,unidade)
+
+        elif opcao == "4":
+
+            listar_fontes(empresa_id)
+
+            fonte_id = int(input("Insira o ID da fonte que deseja desativar: "))
+            desativar_fonte(fonte_id)
+
+        elif opcao == "5":
+            break
+
+        else:
+            print("Opção Inválida.")
+
+def menu_principal(empresa_id):
+
+    while True:
+        print("\n=================================")
+        print("         CARBON TRACK PME          ")
+        print("===================================")
+        print("[1] Cadastre sua empresa")
+        print("[2] Gerencie suas fontes de emissão")
+        print("[0] Sair")
+
+        opcao = input("\nEscolha: ").strip()
+
+        if opcao == "1":
+            cadastrar_empresa()
+
+        elif opcao == "2":
+            menu_fontes(empresa_id)
+
+        elif opcao == "3": 
+            print("Encerrando. Até logo!")
+            break
+
+        else:
+            print("Opção Inválida.")
+
+
+        
+
+
